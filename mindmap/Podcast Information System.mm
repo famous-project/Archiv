e425b2d643ad457b5d50381cb7a103a3df2f56d6
McCouman <support@wikibyte.org>
2013-09-13 20:51:35 +0200

    +mindmap

---
+++ mindmap/Podcast Information System.mm
@@ -0,0 +1,38 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<attribute_registry>
+<attribute_name NAME="PIS" RESTRICTED="true" VISIBLE="true">
+<attribute_value VALUE=""/>
+<attribute_value VALUE="12"/>
+</attribute_name>
+</attribute_registry>
+<node COLOR="#006699" CREATED="1379096392377" ID="ID_1913673789" MODIFIED="1379097567412" TEXT="Podcast Information System">
+<font NAME="SansSerif" SIZE="12"/>
+<attribute_layout NAME_WIDTH="33" VALUE_WIDTH="33"/>
+<attribute NAME="PIS" VALUE="12"/>
+<node CREATED="1379096511904" HGAP="57" ID="ID_1662168500" LINK="person.json.mm" MODIFIED="1379097173590" POSITION="right" TEXT="person.json" VSHIFT="-73">
+<icon BUILTIN="male1"/>
+</node>
+<node CREATED="1379096569591" HGAP="108" ID="ID_1417499361" LINK="base.json.mm" MODIFIED="1379097221725" POSITION="left" TEXT="base.json" VSHIFT="-59">
+<icon BUILTIN="folder"/>
+</node>
+<node CREATED="1379097293141" HGAP="80" ID="ID_929726659" LINK="produce.json.mm" MODIFIED="1379097339059" POSITION="right" TEXT="produce.json" VSHIFT="-60">
+<icon BUILTIN="broken-line"/>
+</node>
+<node CREATED="1379097307257" HGAP="77" ID="ID_879192271" LINK="geo.json.mm" MODIFIED="1379097351907" POSITION="right" TEXT="geo.json" VSHIFT="13">
+<icon BUILTIN="gohome"/>
+</node>
+<node CREATED="1379096626960" HGAP="125" ID="ID_439666802" LINK="meta.json.mm" MODIFIED="1379097386914" POSITION="left" TEXT="meta.json" VSHIFT="-72">
+<icon BUILTIN="bookmark"/>
+</node>
+<node CREATED="1379097249972" HGAP="120" ID="ID_1824841244" LINK="project.json.mm" MODIFIED="1379097393034" POSITION="left" TEXT="project.json" VSHIFT="-11">
+<icon BUILTIN="korn"/>
+</node>
+<node CREATED="1379097319118" HGAP="62" ID="ID_1694019045" LINK="overview,json.mm" MODIFIED="1379097359523" POSITION="right" TEXT="overview,json" VSHIFT="43">
+<icon BUILTIN="calendar"/>
+</node>
+<node CREATED="1379097263579" HGAP="100" ID="ID_760182919" LINK="redirect.json.mm" MODIFIED="1379097391618" POSITION="left" TEXT="redirect.json" VSHIFT="52">
+<icon BUILTIN="launch"/>
+</node>
+</node>
+</map>
---
+++ mindmap/person.json.mm
@@ -0,0 +1,4 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1379096476962" ID="ID_1034188611" LINK="Podcast%20Information%20System.mm" MODIFIED="1379096511877" TEXT="person.json"/>
+</map>
