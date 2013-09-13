7300de740964db41df011efc56373cc3b6bee948
McCouman <support@wikibyte.org>
2013-09-13 22:49:30 +0200

    new base.json + maps

--- mindmap.json/base.json.mm
+++
@@ -1,4 +0,0 @@
-<map version="0.9.0">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1379096558568" ID="ID_1562021062" LINK="Podcast%20Information%20System.mm" MODIFIED="1379096569567" TEXT="base.json"/>
-</map>
