a87eb5fa211731381f23d755b0034aadd3b97379
Simon Waldherr <git@simon.waldherr.eu>
2013-09-16 23:30:18 +0200

    cleanup

--- beispieldateien/mindmap.json/meta.json.mm
+++
@@ -1,4 +0,0 @@
-<map version="0.9.0">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1379096602839" ID="ID_978023134" LINK="Podcast%20Information%20System.mm" MODIFIED="1379096626943" TEXT="meta.json"/>
-</map>
--- beispieldateien/mindmap.json/overview,json.mm
+++
@@ -1,6 +0,0 @@
-<map version="0.9.0">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1379096907312" HGAP="64" LINK="Podcast%20Information%20System.mm" MODIFIED="1379097319103" TEXT="overview,json" VSHIFT="37">
-<icon BUILTIN="calendar"/>
-</node>
-</map>
